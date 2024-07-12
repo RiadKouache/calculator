11df565bdd3b66b25e5ebd594c41e0194dbdb7cf
riadkche <141369946+RiadKouache@users.noreply.github.com>
2024-07-12 20:10:13 +0100

    adding a logic that populate the display with numbers buttons + storing the display value in a variable

--- calculator.js
+++ calculator.js
@@ -14,11 +14,25 @@ function divide(a, b) {
   return a / b;
 }
 
-let a, b, operator;
-
 function operate(operator, a, b) {
   if (operator === "+") add(a, b);
   else if (operator === "-") substract(a, b);
   else if (operator === "*") multiply(a, b);
   else if (operator === "/") divide(a, b);
 }
+
+let a, b, operator;
+let displayValue;
+
+const display = document.querySelector(".display");
+const digits = document.querySelectorAll(".digit");
+
+digits.forEach((e) => {
+  e.addEventListener("click", () => {
+    if (display.textContent === "0") display.textContent = e.textContent;
+    else display.textContent += e.textContent;
+
+    displayValue = display.textContent;
+    console.log(displayValue);
+  });
+});
