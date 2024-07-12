bd67def5acecdb5d3d7391321d3bb5a31c0c5db5
riadkche <141369946+RiadKouache@users.noreply.github.com>
2024-07-12 22:28:12 +0100

    making the calculator work, the next step is for fixing the application to work properly

--- calculator.js
+++ calculator.js
@@ -15,17 +15,22 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-  if (operator === "+") add(a, b);
-  else if (operator === "-") substract(a, b);
-  else if (operator === "*") multiply(a, b);
-  else if (operator === "/") divide(a, b);
+  let result;
+  if (operator === "+") result = add(a, b);
+  else if (operator === "-") result = substract(a, b);
+  else if (operator === "*") result = multiply(a, b);
+  else if (operator === "/") result = divide(a, b);
+
+  return result;
 }
 
-let a, b, operator;
+let a, b, operator, result;
 let displayValue;
 
 const display = document.querySelector(".display");
 const digits = document.querySelectorAll(".digit");
+
+//////////
 
 digits.forEach((e) => {
   e.addEventListener("click", () => {
@@ -35,4 +40,32 @@ digits.forEach((e) => {
     displayValue = display.textContent;
     console.log(displayValue);
   });
+});
+
+/////////////
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((e) => {
+  e.addEventListener("click", () => {
+    operator = e.textContent;
+    console.log(operator);
+    a = Number(displayValue);
+    console.log("first number : ", a);
+    display.textContent = "0";
+  });
+});
+
+const equals = document.querySelector(".equals");
+
+equals.addEventListener("click", (e) => {
+  const equal = e.target.textContent;
+  console.log(equal);
+
+  b = Number(displayValue);
+  console.log("second number : ", b);
+  equals;
+  result = operate(operator, a, b);
+  console.log(result);
+  display.textContent = String(result);
 });
